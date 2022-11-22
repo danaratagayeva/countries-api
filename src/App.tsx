6d5f05afd0f6c90ad76ae8281dd754355c54e9ba
Danara Tagayeva <danara.tagayeva@gmail.com>
2022-11-22 00:35:42 -0500

    add page routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import {BrowserRouter as Router, Routes,Route} from "react-router-dom";
+import About from './pages/about/About';
+import Details from './pages/details/Details';
+import Home from './pages/home/Home';
+import Technologies from './pages/technologies/Technologies';
 
 function App() {
   return (
-    <div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/"  element={<Home />}/>
+        <Route path="/details" element={<Details />}/>
+        <Route path="/about" element={<About />}/>
+        <Route path="/tech" element={<Technologies />}/>
+      </Routes>
+    </Router>
   );
 }
 
